24803a5effcaecfd92b3c91f67ce45cf6748cb1e
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-02-18 14:10:59 +0100

    basic sertup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`);
+});
